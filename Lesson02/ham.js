2906019e5bd30a018a3fbd4b9d09655bde2ea542
SAV <sav.free@yandex.ru>
2020-01-22 19:05:02 +0300

    Commit 1 for L2

---
+++ Lesson02/ham.js
@@ -0,0 +1,92 @@
+/*  Некая сеть фастфуда предлагает несколько видов гамбургеров: маленький
+    (50 рублей, 20 калорий) и большой (100 рублей, 40 калорий).
+    Гамбургер может быть с одной из нескольких видов начинок (обязательно).
+    Дополнительно гамбургер можно посыпать приправой (+15 рублей, +0 калорий)
+    и полить майонезом (+20 рублей, +5 калорий). 
+    Напишите программу, рассчитывающую стоимость и калорийность гамбургера. */
+
+const hams = [
+                {price: 50, calories: 20},
+                {price: 100, calories: 40},
+            ];
+
+const stufs = [
+                {id: 0, name: 'с сыром', price: 10, calories: 20, checked: true},
+                {id: 1, name: 'с салатом', price: 20, calories: 5, checked: true},
+                {id: 2, name: 'с картофелем', price: 15, calories: 10, checked: true},
+                {id: 3, name: 'с приправой', price: 15, calories: 0, checked: false},
+                {id: 4, name: 'с майонезом', price: 20, calories: 5, checked: false}, 
+            ];
+    
+class Hamburger {
+    constructor(size) {
+        this.size = size;
+        this.calories = hams[size].calories;
+        this.price = hams[size].price;
+        this.stuffing = [];
+    }
+    
+    getName() {
+        let name = `${(this.size == 0 ? 'Маленький' : 'Большой')} гамбургер `;
+        let stuffing = '';   
+        for (let i of this.stuffing) {
+            stuffing += (stuffing ==='' ? '' : ', ') + stufs.find(stuf => stuf.id === i).name;
+        }
+        return `${name} (${stuffing})`;
+    }
+
+    addStuffing(id) { // добавить начинку или дополн. ингридиент
+        this.stuffing.push(id)
+    }
+      
+    calculatePrice() { // получить цену
+        let sum = hams[size].price;
+        
+        for (let i of this.stuffing) {
+            sum += stufs.find(stuf => stuf.id === i).price;
+        }       
+        return sum; 
+    }
+    
+    calculateCalories() { // рассчитать калории
+        let sum = hams[size].calories;
+        
+        for (let i of this.stuffing) {
+            sum += stufs.find(stuf => stuf.id === i).calories;
+        }
+        return sum;
+    }
+    
+    renderResult(container) {
+        let result = `${this.getName()}\nСтоимость: ${this.calculatePrice()}\nКол-во калорий: ${this.calculateCalories()}`;
+        document.getElementById(container).innerHTML = result;
+        alert(result);
+    }
+}
+
+function validStuffing() {
+    result = false;
+    
+    for (let stuf of stufs.filter(stuf => stuf.checked)) {
+        result = result || document.getElementById(stuf.id).checked
+    }  
+    return result;
+}
+
+function calculate() {
+    if (!validStuffing()) { // проверка на наличие хотя бы одной начинки
+        alert('Должна быть выбрана хотя бы одна начинка!');
+        return false;
+    }
+    
+    size = 0 + document.getElementById('big').checked; // 0 -small, 1 - big
+    const ham = new Hamburger(size);
+        
+    for (let stuf of stufs) {
+        if (document.getElementById(stuf.id).checked) {
+            ham.addStuffing(stuf.id);
+        }        
+    }
+    // выводим результат
+    ham.renderResult('result');
+}
--- Lesson02/main.js
+++ Lesson02/main.js
@@ -12,51 +12,105 @@ class GoodsItem {
         return `<div class="goods-item" data-id="${this.id}"><img src="${this.img}" alt="${this.name}" title="${this.name}">
                 <h3>${this.name}</h3><p>Цена: <b>${formatter.format(this.price)} ₽</b></p>
                 <button class="add-button" title="Добавить товар в корзину"
-                    onclick="addGoodToCart()">Добавить</button></div>`;
+                    onclick="addGoodToCart(${this.id})">Добавить</button></div>`;
     }
 }
 
 class GoodsList {
     constructor(container) {
-        this.container = document.querySelector(container);
-        
+        this.container = document.querySelector(container);        
         this.goods = [];
     }
     
     fetchGoods() {
         this.goods = [
-            { id:1, title: "Товар 1", price: 20000},
-            { id:2, title: "Товар 2", price: 21500},
-            { id:3, title: "Товар 3", price: 32000},
-            { id:4, title: "Товар 4", price: 26000},
-            { id:5, title: "Товар 5", price: 18000},
-            { id:6, title: "Товар 6", price: 34500},
-            { id:7, title: "Товар 7", price: 10000},
-            { id:8, title: "Товар 8", price: 25000},
-            { id:9, title: "Товар 9", price: 20000},
-            { id:10, title: "Товар 10", price: 5500},
-            { id:11, title: "Товар 11", price: 8000},
-            { id:12, title: "Товар 12", price: 12000},
-            { id:13, title: "Товар 13", price: 11700},
+            { id:1, name: "Товар 1", price: 20000},
+            { id:2, name: "Товар 2", price: 21500},
+            { id:3, name: "Товар 3", price: 32000},
+            { id:4, name: "Товар 4", price: 26000},
+            { id:5, name: "Товар 5", price: 18000},
+            { id:6, name: "Товар 6", price: 34500},
+            { id:7, name: "Товар 7", price: 10000},
+            { id:8, name: "Товар 8", price: 25000},
+            { id:9, name: "Товар 9", price: 20000},
+            { id:10, name: "Товар 10", price: 5500},
+            { id:11, name: "Товар 11", price: 8000},
+            { id:12, name: "Товар 12", price: 12000},
+            { id:13, name: "Товар 13", price: 11700},
         ];  
+    }
+    
+    fetchGood(id) {
+        return this.goods.find(good => good.id === id);
     }
     
     render() {
         let goodsList = '';
         this.goods.forEach(good => {
-            const goodsItem = new GoodsItem(good.id, good.title, good.price, good.img);
+            const goodsItem = new GoodsItem(good.id, good.name, good.price, good.img);
             goodsList += goodsItem.render();
         });
         this.container.innerHTML = goodsList;
-        //this.initListeners();
     }  
 }
 
-function addGoodToCart() {
-    alert('Товар добавлен в корзину!');
+class Cart {
+    constructor() {
+        // ...cookie
+        this.goods = []; // {id, n}
+    }
+    
+    renderCartButton() {
+        let button = document.getElementById('cart-button');
+        button.innerHTML = `Корзина (${this.goods.length})` 
+        button.title = `${this.sum()} ₽`; // пересчитываем стоимость корзины
+        return this.goods.length;
+    }
+    
+    addGood(id) {
+        //alert('goods(1): ' + JSON.stringify(this.goods));
+        let index = this.goods.findIndex(good => good.id === id);
+        if (index < 0) { // товар не найден (товара нет в корзине)
+            this.goods.push({id: id, n: 1}); // добавляем товар в корзину
+        } else { // товар уже есть в корзине
+            this.goods[index].n += 1; // увеличиваем кол-во товара
+        } 
+
+        this.renderCartButton(); // обновляем кол-во товаров в корзине (надпись на кнопке)
+        //alert('goods(2): ' + JSON.stringify(this.goods));  
+    }
+    
+    viewCart() { // просмотр корзины   
+        let s = 'Корзина:\n';
+        for (let item of this.goods) {
+            let good = list.fetchGood(item.id);
+            s += `${good.name} - ${item.n} шт. * ${good.price} ₽ = ${good.price * item.n}  ₽\n`;
+        }
+        s += `-------------------------\nИтого: ${this.sum()} ₽`;
+        alert(s);        
+    }
+    
+    sum() { // подсчёт стоимости корзины
+        let sum = 0;
+        for (let item of this.goods) {
+            let good = list.fetchGood(item.id);
+            sum += item.n * good.price;
+        }
+        return sum;
+    }
+}
+
+function addGoodToCart(id) {    
+    cart.addGood(id); // добавляем товар в корзину
+    let good = list.fetchGood(id);
+    document.getElementById('msg-line').innerHTML=`${good.name} добавлен в корзину!`;
+    let div = document.getElementById('msg-line');
+    div.style.display = 'block';
+    setTimeout(() => div.style.display = 'none', 5000);
 }
 
 
 const list = new GoodsList('.goods-list');
+const cart = new Cart();
 list.fetchGoods();
 list.render();
