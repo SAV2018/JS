0ecab99dc60db8c943a8647bb99ebd500699e76f
SAV <sav.free@yandex.ru>
2020-02-03 01:42:21 +0300

    Lesson-5 Final commit

--- Lesson05/main.js
+++ Lesson05/main.js
@@ -1,275 +1,158 @@
-function replaceQuotes() {
-/*  Дан большой текст, в котором для оформления прямой речи используются
-    одинарные кавычки. Придумать шаблон, который заменяет одинарные кавычки
-    на двойные так, чтобы в конструкциях типа aren't одинарная кавычка не
-    заменялась на двойную. */
+const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-    const s1 = "'In vino veritas.' Д'Артаньян сказал: 'А пошли вы все на!..'\nВсе ответили: 'Да пошёл ты сам!' И всё стало о'k.\n'Don't stop it!'";
-    const s2 = s1.replace(/(?<![a-zA-Z0-9А-Яа-яЁё])'.*?/g, '"');
-    alert(s1 + '\n\n' + s2);
-}
-//------------------------------------------------------------------------------
+const app = new Vue({
+    el: '#app',
+    data: {
+        cart: [],           // список товаров в корзине
+        goods: [],          // список всех товаров
+        filteredGoods: [],  // список вывода товаров
+        pattern: ''         // шаблон поиска
+    },
+    computed: {
+        getImage(good) {
+            if (!good.img) {
+                good.img = 'https://via.placeholder.com/150';
+            }
+            return good.img;
+        },
+        
+        isCartMode() { // режим корзины
+            return (window.location.search.lastIndexOf('cart') !== -1)
+        },
+        
+        cartSize() { // кол-во товаров в корзине (разных наименований)
+            return this.cart.length;
+        },
+        
+        getCart() {
+            let goods = this.cart;
 
-const API_URL = "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses";
-
-function makeXMLHttpRequest(url, callback) {
-    let xhr;
-     
-    if (window.XMLHttpRequest) {
-        xhr = new window.XMLHttpRequest();
-    } else if (window.ActiveXObject) {
-        xhr = new window.ActiveXObject('Microsoft.XMLHTTP');
-    }
-    
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            //alert('readyState = '+xhr.readyState+' xhr.status = '+ xhr.status+' responseText = '+xhr.responseText);
-            callback(xhr.responseText);
+            goods.forEach((good, index) => {
+                const item = this.fetchGood(good.id);
+                goods[index].price = item.price;
+                goods[index].name = item.product_name;
+            });
+            return goods;
+        },
+        
+        emptyGoodsList() {
+            return !(this.filteredGoods.length > 0);
+        },
+        
+        emptyCart() {
+            return !(this.cart.length > 0)
         }
-    }
+    },
+    methods: { 
+        makeXHRequest(url) {
+            return new Promise((resolve, reject) => {
+                let xhr;
+                if (window.XMLHttpRequest) {
+                    xhr = new window.XMLHttpRequest();
+                } else  {
+                    xhr = new window.ActiveXObject("Microsoft.XMLHTTP");
+                }
+                
+                xhr.onreadystatechange = function() {            
+                    if (xhr.readyState === 4) {
+                        if (xhr.status === 200) {                    
+                            resolve(JSON.parse(xhr.responseText)); 
+                        } else {
+                            reject(xhr.responseText);
+                        }
+                    }
+                }
+                
+                xhr.timeout = 5000;
+                xhr.ontimeout = function () {
+                    reject('Timeout');
+                }
+                
+                xhr.onerror = function (err) {
+                    reject(err);
+                };
+                
+                xhr.open('GET', url);
+                xhr.send();
+            });
+        },
+ 
+        async fetchGoods() { // получение списка товаров
+            try {
+                this.goods = await this.makeXHRequest(`${API_URL}/catalogData.json`)
+                this.filteredGoods = [...this.goods];
+            } catch (e) {
+                console.error(e);
+            }
+        },
+                
+        fetchGood(id) { // получение товара по id
+            return this.goods.find(good => good.id_product === id);
+        },
+
+        addGood(id) { // добавление товара в корзину
+            let index = this.cart.findIndex(good => good.id === id);
+            if (index < 0) { // товар не найден (товара нет в корзине)
+                this.cart.push({id: id, n: 1}); // добавляем товар в корзину
+            } else { // товар уже есть в корзине
+                this.cart[index].n += 1; // увеличиваем кол-во товара на 1
+            }
     
-    xhr.timeout = 5000;
-    xhr.ontimeout = function () {
-        alert('Timeout');
-    }
-
-    xhr.open('GET', url);
-    xhr.send();
-}
-
-const callback = function viewResponse(text) {
-    let goods= JSON.parse(text);
-
-    let s = 'Ответ сервера:\n\nID | Название товара | Цена\n--------------------------------\n';
-    for (let item of goods) {
-        const good = list.fetchGood(item.id_product);
-        s += `${good.id_product} | ${good.product_name} | ${good.price.toLocaleString('ru-RU')} ₽\n`;
-    }
-    s += '--------------------------------\n';
-    alert(s);
-}
-
-//------------------------------------------------------------------------------
-
-function makeRequest(url) {
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-    
-        xhr.onreadystatechange = function() {            
-            if (xhr.readyState === 4) {
-                //alert('readyState = '+xhr.readyState+' xhr.status = '+ xhr.status+' responseText = '+xhr.responseText);
-                if (xhr.status === 200) {                    
-                    resolve(xhr.responseText)
-                } else {
-                    reject(xhr.status)
+            this.update();
+        },
+        
+        decGood(id) { // уменьшение кол-ва товара в корзине
+            let index = this.cart.findIndex(good => good.id === id);
+            if (index >= 0) { // товар найден (товара в корзине)
+                this.cart[index].n -= 1; // уменьшаем кол-во товара на 1
+                if (this.cart[index].n === 0) { // если кол-во товара = 0
+                    this.removeGood(id); // удаляем товар из корзины
                 }
             }
-        }
-        
-        xhr.timeout = 5000;
-        xhr.ontimeout = function () {
-            reject('Timeout');
-        }
-        xhr.open('GET', url);
-        xhr.send();
-    });
-}
-
-function usePromise() {
-    const request = makeRequest(`${API_URL}/catalogData.json`);
-    request
-        .then((response) => callback(response))
-        .catch((error) => reject(error))
-}
-
-const reject = function rejectResponse(error) {
-    alert(error);
-}
-
-
-//------------------------------------------------------------------------------
-
-class GoodsItem {
-    constructor(id, name, price, img = 'https://via.placeholder.com/150') {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.img = img;
-    }
-
-    render() {
-        return `<div class="goods-item" data-id="${this.id}"><img src="${this.img}" alt="${this.name}" title="${this.name}">
-                <h3>${this.name}</h3><p>Цена: <b>${this.price.toLocaleString('ru-RU')} ₽</b></p>
-                <button class="add-button" title="Добавить товар в корзину"
-                    onclick="addGoodToCart(${this.id})">Добавить</button></div>`;
-    }
-}
-
-class GoodsList {
-    constructor(container) {
-        this.container = document.querySelector(container);        
-        this.goods = [];
-        this.filteredGoods = [];
-        
-        const searchForm = document.getElementById('search-form');
-
-        searchForm.addEventListener('submit', (e) => {
-            e.preventDefault();
-            const pattern = document.getElementById('search-pattern').value.trim();
-            if (pattern !== '') this.filterGoods(pattern);                
-        });
-    }
-        
-    filterGoods(pattern) {
-        const rex = new RegExp(pattern, 'i');
-        this.filteredGoods = this.goods.filter((good) => {
-            return rex.test(good.product_name, pattern);                                  
-        });
-        this.render();
-    }
+            
+            this.update();
+        },
     
-    fetchGoods() {
-        this.goods = [
-            { id_product:1, product_name: "Товар 1", price: 20000},
-            { id_product:2, product_name: "Товар 2", price: 21500},
-            { id_product:3, product_name: "Товар 3", price: 32000},
-            { id_product:4, product_name: "Товар 4", price: 26000},
-            { id_product:5, product_name: "Товар 5", price: 18000},
-            { id_product:6, product_name: "Товар 6", price: 34500},
-            { id_product:7, product_name: "Товар 7", price: 10000},
-            { id_product:8, product_name: "Товар 8", price: 25000},
-            { id_product:9, product_name: "Товар 9", price: 20000},
-            { id_product:10, product_name: "Товар 10", price: 5500},
-            { id_product:11, product_name: "Товар 11", price: 8000},
-            { id_product: 123, product_name: "Ноутбук", price: 45600 },
-            { id_product: 456, product_name: "Мышка", price: 1000 },
-        ];
-        this.filteredGoods = [...this.goods];
-    }
-    
-    fetchGood(id) {
-        return this.goods.find(good => good.id_product === id);
-    }
-    
-    render() {
-        let goodsList = '';
-        if (this.filteredGoods.length > 0) {            
-            this.filteredGoods.forEach(good => {
-                const goodsItem = new GoodsItem(good.id_product, good.product_name, good.price, good.img);
-                goodsList += goodsItem.render();
-            });            
-        } else { // список товаров пуст
-            goodsList += '<br />Не найдено товаров.<br /><br />';
-        }
-        this.container.innerHTML = goodsList;
-    }  
-}
-
-class Cart {
-    constructor(container) {
-        this.container = document.querySelector(container);
-        
-        this.goods = [];
-        if (localStorage.getItem('cart') !== null) {
-            this.goods = JSON.parse(localStorage.getItem('cart'));
-        }
-        this.renderCartButton();
-    }
-    
-    renderCartButton() {
-        let button = document.getElementById('cart-button');
-        
-        button.innerHTML = `Корзина (${this.goods.length})`;
-        button.title = `${this.sum()} ₽`; // пересчитываем стоимость корзины
-        return this.goods.length;
-    }
-    
-    addGood(id) {
-        //alert('goods(1): ' + JSON.stringify(this.goods));
-        let index = this.goods.findIndex(good => good.id === id);
-        if (index < 0) { // товар не найден (товара нет в корзине)
-            this.goods.push({id: id, n: 1}); // добавляем товар в корзину
-        } else { // товар уже есть в корзине
-            this.goods[index].n += 1; // увеличиваем кол-во товара на 1
-        }
-
-        this.update();
-    }
-    
-    decGood(id) {
-        let index = this.goods.findIndex(good => good.id === id);
-        if (index >= 0) { // товар найден (товара в корзине)
-            this.goods[index].n -= 1; // уменьшаем кол-во товара на 1
-            if (this.goods[index].n === 0) { // если кол-во товара = 0
-                this.removeGood(id); // удаляем товар из корзины
+        removeGood(id) { // удаление товара из корзины
+            let index = this.cart.findIndex(good => good.id === id);
+            
+            if (index >= 0) { // товар найден (товара в корзине)
+                this.cart.splice(index, 1); // удаляем товар
             }
-        }
-        
-        this.update();
-    }
+            
+            this.update();
+        },
+          
+        update() { // сохранение корзины
+            localStorage.setItem('cart', JSON.stringify(this.cart));   
+        },
     
-    removeGood(id) {
-        let index = this.goods.findIndex(good => good.id === id);
+        sum() { // подсчёт стоимости товаров в корзине
+            let sum = 0;
+            
+            for (let item of this.cart) {
+                let good = this.fetchGood(item.id);
+                sum += item.n * good.price;
+            }
+            return sum;
+        },
         
-        if (index >= 0) { // товар найден (товара в корзине)
-            this.goods.splice(index, 1); // удаляем товар
-        }
+        findGood() { // поиск товара
+            if (this.pattern !== '') this.filterGoods(this.pattern);
+        },
         
-        this.update();
-    }
-    
-    update() {
-        localStorage.setItem('cart', JSON.stringify(this.goods));
-        this.renderCartButton(); // обновляем кол-во товаров в корзине (надпись на кнопке)
-        if (isCartMode()) { // если режим просмотра корзины
-            this.render(); // перерисовываем корзину
-        }        
-    }
+        filterGoods(pattern) { // получение списка товаров по шаблону
+            const rex = new RegExp(pattern, 'i');
+            this.filteredGoods = this.goods.filter((good) => {
+                return rex.test(good.product_name, pattern);                                  
+            });
+        }
+    },
+    mounted() {
+        this.fetchGoods();
         
-    render() {
-        let s = '<h2>Корзина:</h2><hr />';
-        for (let item of this.goods) {
-            const good = list.fetchGood(item.id);
-            s += `<div class="cart-item"><button class="cart-button" title="Увеличить кол-во товара в корзине"
-                    onclick="addGoodToCart(${item.id})">+</button><button class="cart-button" title="Уменьшить кол-во товара в корзине"
-                    onclick="cart.decGood(${item.id})">-</button><button class="cart-button-red" title="Удалить товар из корзины"
-                    onclick="cart.removeGood(${item.id})">х</button> <b>${good.product_name}</b> - ${item.n} шт. * ${good.price.toLocaleString('ru-RU')} ₽ = ${(good.price * item.n).toLocaleString('ru-RU')}  ₽</div>`;
+        if (localStorage.getItem('cart') !== null) {
+            this.cart = JSON.parse(localStorage.getItem('cart'));
         }
-        s += `<hr /><div class="cart-cost">Итого: ${this.sum().toLocaleString('ru-RU')} ₽</div><div><a href="index.html" style="margin: 5px 0; font-weight:bold">« Вернуться к товарам</a></div>`;
-        this.container.innerHTML = s;
     }
-    
-    sum() { // подсчёт стоимости корзины
-        let sum = 0;
-        for (let item of this.goods) {
-            let good = list.fetchGood(item.id);
-            sum += item.n * good.price;
-        }
-        return sum;
-    }
-}
-
-function addGoodToCart(id) {    
-    cart.addGood(id); // добавляем товар в корзину
-    const good = list.fetchGood(id);
-    document.getElementById('msg-line').innerHTML=`${good.product_name} добавлен в корзину!`;
-    let div = document.getElementById('msg-line');
-    div.style.display = 'block';
-    setTimeout(() => div.style.display = 'none', 5000);
-}
-
-function isCartMode() {
-    return (window.location.search.lastIndexOf('cart') !== -1)
-}
-
-
-
-const list = new GoodsList('.goods-list');
-list.fetchGoods();
-const cart = new Cart('.goods-list');
-
-if (isCartMode()) {
-    cart.render();
-} else {
-    list.render();
-}
+});
